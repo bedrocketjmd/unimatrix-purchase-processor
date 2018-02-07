44f46f1dcc5dc817bfd708d2ccc007108dd780ca
Stefan Hartmann <stefanhartmann@gmail.com>
2018-02-07 18:51:11 -0500

    DLRPAY-774 set up gem files

---
+++ lib/purchase_processor.rb
@@ -0,0 +1,9 @@
+module Unimatrix
+  module PurchaseProcessor
+    GEM_ROOT = File.expand_path( '../..', __FILE__ )
+  end
+end
+
+require 'adapter/free/free_adapter'
+require 'adapter/paypal/paypal_adapter'
+require 'adapter/stripe/stripe_adapter'
