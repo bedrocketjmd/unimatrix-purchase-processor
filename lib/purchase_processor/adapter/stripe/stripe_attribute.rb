c961aca5fd58234f7ac0a0512257f17549b9f6b0
Stefan Hartmann <stefanhartmann@gmail.com>
2018-02-09 14:55:10 -0500

    Add missing adapter

--- lib/purchase_processor/adapter/stripe/stripe_attribute.rb
+++ lib/purchase_processor/adapter/stripe/stripe_attribute.rb
@@ -1,214 +1,252 @@
 module Unimatrix
   module PurchaseProcessor
     class StripeAttribute < Adapter
-      include CurrencyHelper
+      def attributes_from_webhook( event )
+        object = event.data.object
+        object_name = object.object
 
-      #-------------------------------------------------------------------------------
-      # class methods
+        attributes = {}
 
-      def self.retrieve_key( realm_uuid )
-        test_setting = Rails.cache.fetch(
-          [ realm_uuid, '_dealer_test_setting' ],
-          expires_in: 10.minutes
-        ) do
-
-          settings = Regent.retrieve_settings( realm_uuid )
-
-          if settings
-            settings.detect do | setting |
-              setting[ 'name' ] == 'com.boxxspring.payments.test'
-            end
-          end
+        if object.try( :subscription )
+          payments_subscription = PaymentsSubscription.find_by( provider_id: object.subscription )
+          attributes[ :payments_subscription_id ] = payments_subscription.id
+          attributes[ :customer_product_id ] = payments_subscription.customer_product.id
         end
 
-        if test_setting
-          ENV[ 'STRIPE_TEST_SECRET_KEY' ]
+        if object.try(:lines)
+          metadata = object.lines.first.metadata
         else
-          ENV[ 'STRIPE_SECRET_KEY' ]
+          metadata = object.metadata
+        end
+
+        if metadata.present? && metadata.to_h.present?
+          attributes.merge!( StripeAdapter.attributes_from_metadata( metadata ) )
+        end
+
+        unless event.type.include?( 'failed' )
+          if [ 'charge', 'invoice', 'dispute', 'subscription' ].include? object_name
+            attributes = StripeAdapter.send( "attributes_from_#{ object_name }", object ).
+                           merge( attributes )
+          end
+
+          if attributes[ :tax ].nil?
+            tax = PurchaseTransaction.where( provider_id: object.charge ).first.tax
+            attributes[ :tax ] = tax
+          end
+
+          StripeAdapter.approximate_missing_values( attributes )
+        else
+          attributes
         end
       end
 
-      def self.transaction_from_webhook( event, provider_subscription=nil )
-        adapter ||= StripeSubscription.new
-        adapter.transaction_from_webhook( event )
+      def attributes_from_metadata( object )
+        begin
+          if object.try( :realm_id )
+            realm_id = Realm.find_by( id: object.realm_id.to_i ).id
+          elsif object.try( :realm_uuid )
+            realm_id = Realm.find_by( uuid: object.realm_uuid ).id
+          else
+            realm_id = nil
+          end
+
+          attributes = {
+            realm_id: realm_id,
+            offer_id: object.offer_id.to_i,
+            product_id: object.product_id.to_i,
+            customer_id: object.customer_id.to_i,
+            device_platform: object.device_platform,
+            provider: object.provider,
+            subtotal: object.subtotal.to_f,
+            subtotal_usd: object.subtotal_usd.to_f,
+            tax: object.tax.to_f,
+            total: object.total.to_f
+          }
+
+          attributes
+        rescue
+          {}
+        end
       end
 
-      def self.attributes_from_webhook( event )
-        adapter ||= StripeAttribute.new
-        adapter.attributes_from_webhook( event )
+      def attributes_from_invoice( object )
+        begin
+          attributes = {}
+
+          if object.charge.present?
+            charge = Stripe::Charge.retrieve( object.charge )
+            attributes.merge!( StripeAdapter.attributes_from_charge( charge ) )
+          end
+          subscription = object.lines.data.select { | item | item.type == 'subscription' }
+          subscription = subscription.first
+          attributes.merge!( StripeAdapter.attributes_from_metadata( subscription.metadata ) )
+          exponent = exponent_from_currency( object.currency )
+
+          attributes.merge(
+            subtotal:          StripeAdapter.format_stripe_money( object.subtotal, exponent ),
+            tax:               StripeAdapter.format_stripe_money( object.tax, exponent ),
+            tax_percent:       object.tax_percent,
+            total:             StripeAdapter.format_stripe_money( object.total, exponent )
+          )
+        rescue
+          {}
+        end
       end
 
-      def self.attributes_from_metadata( object )
-        adapter ||= StripeAttribute.new
-        adapter.attributes_from_metadata( object )
+      def attributes_from_dispute( object )
+        begin
+          exponent = exponent_from_currency( object.currency )
+          attributes = {
+            response_code: object.reason,
+            response_message: object.status,
+            subtotal: StripeAdapter.format_stripe_money( object.amount, exponent )
+          }
+          related_transaction = nil
+
+          if object.charge.present?
+            charge = Stripe::Charge.retrieve( object.charge )
+            attributes.merge!( StripeAdapter.attributes_from_charge( charge ) )
+            related_transaction = StripeAdapter.related_transaction_from_charge( charge.id )
+          end
+
+          if related_transaction.present?
+            attributes[:transaction_id] = related_transaction.id
+            attributes.merge!( StripeAdapter.attributes_from_related_transaction( related_transaction ) )
+          end
+
+          if object.balance_transactions.present?
+            balance_transaction = object.balance_transactions.first
+            attributes.merge!( StripeAdapter.attributes_from_balance_transaction( balance_transaction ) )
+          end
+
+          attributes
+        rescue
+          {}
+        end
       end
 
-      def self.subscription_from_event( event )
-        adapter ||= StripeSubscription.new
-        adapter.subscription_from_event( event )
+      def attributes_from_charge( object )
+        balance_transaction = nil
+
+        if object.source.object == 'source'
+          card = object.source.card
+        else
+          card = object.source
+        end
+
+        attributes = {
+          currency:            object.currency.upcase,
+          provider_id:         object.id,
+          payment_method:      card.brand,
+          payment_identifier:  card.last4,
+          total:               StripeAdapter.format_stripe_money( object.amount,
+                                             exponent_from_currency( object.currency ) )
+        }
+
+        if object.balance_transaction
+          balance_transaction = Stripe::BalanceTransaction.
+                                    retrieve( object.balance_transaction )
+        end
+
+        attributes.merge( StripeAdapter.attributes_from_balance_transaction( balance_transaction ) )
       end
 
-      def self.interpret_webhook( event, transaction, stripe_subscription, relation )
-        adapter ||= StripeSubscription.new
-        adapter.interpret_webhook( event, transaction, stripe_subscription, relation )
-      end
+      def attributes_from_balance_transaction( balance_transaction )
+        result = {}
 
-      def self.transaction_valid?( transaction )
-        transaction.present? && transaction.valid?
-      end
+        if balance_transaction.present?
 
-      def self.pause_subscription( subscription )
-        adapter = StripeSubscription.new
-        adapter.pause_subscription( subscription )
-      end
+          default_exponent = exponent_from_currency( balance_transaction.currency )
 
-      def self.resume_subscription( subscription )
-        adapter = StripeSubscription.new
-        adapter.resume_subscription( subscription )
-      end
-
-      def create_plan( offer, realm )
-        adapter = StripeSubscription.new
-        adapter.create_plan( offer, realm )
-      end
-
-      def self.find_webhook_event( event_id )
-        Stripe::Event.retrieve( event_id )
-      end
-
-      def self.valid_events
-        [ 'invoice.payment_succeeded', 'invoice.payment_failed', 'customer.subscription.deleted',
-          'charge.dispute.created', 'charge.dispute.funds_reinstated', 'charge.dispute.funds_withdrawn',
-          'charge.dispute.updated','charge.dispute.closed' ]
-      end
-
-      #-------------------------------------------------------------------------------
-      # instance methods
-
-      def refresh_api_key( realm )
-        Stripe.api_key = StripeAdapter.retrieve_key( realm.uuid )
-      end
-
-      def provider_customer( customer  )
-        @provider_customer ||= StripeCustomer.find_or_create( customer )
-      end
-
-      def customer_valid?( customer )
-        customer.present? && provider_customer( customer ) && provider_customer( customer ).id.present?
-      end
-
-      def transation_valid?( transaction )
-        transaction.present? && transaction.valid?
-      end
-
-      def create_charge( customer:, amount:, offer:, currency:, metadata:, request_attributes: nil, source_type: )
-        adapter ||= StripeCharge.new
-        adapter.create_charge(
-          customer: customer,
-          amount: amount,
-          offer: offer,
-          currency: currency,
-          metadata: metadata,
-          request_attributes: nil,
-          source_type: source_type,
-          stripe_customer: provider_customer( customer )
-        )
-      end
-
-      def new_refund_transaction( reference_transaction, refund_attributes )
-        adapter ||= StripeRefund.new
-        adapter.new_refund_transaction( reference_transaction, refund_attributes )
-      end
-
-      def new_subscription( customer, device_platform, offer )
-        adapter = StripeSubscription.new
-        adapter.new_subscription( customer, device_platform, offer )
-      end
-
-      def charge_successful?( charge )
-        adapter ||= StripeCharge.new
-        adapter.charge_successful?( charge )
-      end
-
-      def subscription_successful?( subscriber )
-        adapter ||= StripeSubscription.new
-        adapter.subscription_successful?( subscriber )
-      end
-
-      def pay_delinquent_subscription( payments_subscription )
-        adapter ||= StripeSubscription.new
-        adapter.pay_delinquent_subscription( payments_subscription )
-      end
-
-      def new_purchase_transaction( attributes )
-        StripePurchaseTransaction.new( attributes.merge( provider: 'Stripe' ) )
-      end
-
-      def self.generate_discount_coupon( discount, currency )
-        discount_coupon = Stripe::Coupon.create(
-          id: "#{ SecureRandom.hex }-discount_coupon",
-          duration: "once",
-          amount_off: ( discount * 100 ).to_i,
-          currency: currency
-        )
-
-        discount_coupon
-      end
-
-      private
-
-      def self.stripe_customer( customer )
-        adapter ||= StripeCustomer.new
-        adapter.stripe_customer( customer )
-      end
-
-      def self.attributes_from_invoice( object )
-        adapter ||= StripeAttribute.new
-        adapter.attributes_from_invoice( object )
-      end
-
-      def self.attributes_from_dispute( object )
-        adapter ||= StripeAttribute.new
-        adapter.attributes_from_dispute( object )
-      end
-
-      def self.attributes_from_charge( object )
-        adapter ||= StripeAttribute.new
-        adapter.attributes_from_charge( object )
-      end
-
-      def self.attributes_from_balance_transaction( balance_transaction )
-        adapter ||= StripeAttribute.new
-        adapter.attributes_from_balance_transaction( balance_transaction )
-      end
-
-      def self.attributes_from_subscription( object )
-        adapter ||= StripeAttribute.new
-        adapter.attributes_from_subscription( object )
-      end
-
-      def self.related_transaction_from_charge( charge )
-        PurchaseTransaction.where( provider_id: charge ).first rescue nil
-      end
-
-      def self.attributes_from_related_transaction( related_transaction )
-        adapter ||= StripeAttribute.new
-        adapter.attributes_from_related_transaction( related_transaction )
-      end
-
-      def self.approximate_missing_values( attributes )
-        adapter ||= StripeAttribute.new
-        adapter.approximate_missing_values( attributes )
-      end
-
-      def self.format_stripe_money( cents, exponent )
-        result = 0.0
-
-        if cents.present?
-          result = cents.to_f / exponent.to_f
+          result = {
+            processing_fee_usd: StripeAdapter.format_stripe_money( balance_transaction.fee, default_exponent ) * -1,
+            total_usd:          StripeAdapter.format_stripe_money( balance_transaction.amount, default_exponent )
+          } rescue {}
         end
 
         result
+      end
+
+      def attributes_from_subscription( object )
+        begin
+          expiry = Time.at( object.current_period_end )
+          at_period_end = object.cancel_at_period_end
+
+          attributes = {
+            stripe_subscription_expires_at: expiry,
+            stripe_subscription_at_period_end: at_period_end
+          }
+          attributes.merge!( StripeAttributes.attributes_from_metadata( object.metadata ) )
+
+        rescue
+          {}
+        end
+      end
+
+      def attributes_from_related_transaction( related_transaction )
+        {
+          realm_id: related_transaction.realm_id,
+          offer_id: related_transaction.offer_id,
+          product_id: related_transaction.product_id,
+          customer_id: related_transaction.customer_id,
+          tax: related_transaction.tax,
+          tax_usd: related_transaction.tax_usd,
+          tax_percent: related_transaction.tax_percent,
+          subtotal_usd: related_transaction.subtotal_usd
+        }
+      end
+
+      def approximate_missing_values( attributes )
+        if attributes[ :currency ].present?
+          # Discount must be recalculated to account for any rounding
+          # that may have been done by the provider.
+          attributes[ :discount ] = attributes[ :subtotal ] -
+                                    attributes[ :total ] +
+                                    attributes[ :tax ]
+
+          if attributes[ :discount ].round( 2 ).to_f == 0.0
+            attributes[ :discount ] = 0.0
+          end
+
+          if attributes[ :currency ] == 'USD'
+            attributes[ :subtotal_usd ] =      attributes[ :subtotal ]
+            attributes[ :discount_usd ] =      attributes[ :discount ]
+            attributes[ :tax_usd ] =           attributes[ :tax ]
+
+            # Processing fee is reversed because that's how it's returned from Stripe
+            attributes[ :processing_fee ] =    attributes[ :processing_fee_usd ]
+
+            # processing_fee is a negative number
+            revenue = attributes[ :subtotal ] -
+                      attributes[ :discount ] +
+                      attributes[ :processing_fee ]
+
+            attributes[ :total_revenue ] =     revenue
+            attributes[ :total_revenue_usd ] = revenue
+          else
+            total_usd = attributes[ :total_usd ]
+            total = attributes[ :total ]
+
+            attributes[ :subtotal_usd ] =      exchange_by_ratio( attributes[ :subtotal ], total, total_usd, 'USD' )
+            attributes[ :discount_usd ] =      exchange_by_ratio( attributes[ :discount ], total, total_usd, 'USD' )
+            attributes[ :tax_usd ] =           exchange_by_ratio( attributes[ :tax ], total, total_usd, 'USD' )
+
+            # Processing fee is reversed because that's how it's returned from Stripe
+            attributes[ :processing_fee ] =    exchange_by_ratio( attributes[ :processing_fee_usd ], total, total_usd )
+
+
+            # processing_fee and processing_fee_usd are negative numbers
+            attributes[ :total_revenue ] =     attributes[ :subtotal ] -
+                                               attributes[ :discount ] +
+                                               attributes[ :processing_fee ]
+
+            attributes[ :total_revenue_usd ] = attributes[ :subtotal_usd ] -
+                                               attributes[ :discount_usd ] +
+                                               attributes[ :processing_fee_usd ]
+          end
+        end
+
+        attributes
       end
     end
   end
