80caaadda4a127402ac3d79d87cf900f0ab31eae
Stefan Hartmann <stefanhartmann@gmail.com>
2018-02-09 14:08:42 -0500

    DLRPAY-775 update config methods

--- lib/purchase_processor/adapter/adapter.rb
+++ lib/purchase_processor/adapter/adapter.rb
@@ -7,9 +7,20 @@ module Unimatrix
       end
 
       def self.local_product( resource )
-        product_name = configuration.local_product_name
+        product_name = set_local_product_name
         if resource.respond_to?( product_name )
           resource.send( product_name )
+        end
+      end
+
+      def self.set_local_product_name
+        app_name = configuration.application_name.to_sym
+        unless app_name.nil?
+          app_products = {
+            merchant: 'realm_product',
+            dealer:   'customer_product'
+          }
+          app_products[ app_name ]
         end
       end
     end
