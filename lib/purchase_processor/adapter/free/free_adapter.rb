1a538a2604b96cc53e88734e4ca2d58c31940dc0
Stefan Hartmann <stefanhartmann@gmail.com>
2018-02-09 12:14:53 -0500

    remove commented code

--- lib/purchase_processor/adapter/free/free_adapter.rb
+++ lib/purchase_processor/adapter/free/free_adapter.rb
@@ -1,19 +1,6 @@
 module Unimatrix
   module PurchaseProcessor
     class FreeAdapter < Adapter
-      # class Subscription
-      #   def customer_product
-      #     puts "this is the customer product!!"
-      #   end
-      #   def realm_product
-      #     puts "this is the realm_product!!"
-      #   end
-      # end
-      #
-      # def self.test_thing
-      #   s = Subscription.new
-      #   local_product( s )
-      # end
     end
   end
 end
